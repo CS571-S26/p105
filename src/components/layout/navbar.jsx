d4ea102782ae9454b655a077c6041dc93543ee39
Eric Du <ezdu@wisc.edu>
2026-03-30 10:50:44 -0700

    react aria

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,18 +1,29 @@
+import {
+  Link,
+  Button,
+  MenuTrigger,
+  Popover,
+  Menu,
+  MenuItem,
+} from "react-aria-components";
 import { NavLink } from "react-router";
+import logo from "../../assets/logo.gif";
 
 export default function Navbar() {
   return (
-    <nav>
-      <p className="text-3xl font-bold text-blue-500">HGello Tailwidn</p>
-      <NavLink to="/" end>
-        Home
-      </NavLink>
-      <NavLink to="/artwork" end>
-        Artwork
-      </NavLink>
-      <NavLink to="/about" end>
-        About
-      </NavLink>
+    <nav className="flex">
+      <Link href="/">
+        <img src={logo} alt="logo"></img>
+      </Link>
+
+      <ul>
+        <li>
+          <NavLink to="/artwork">Artwork</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
